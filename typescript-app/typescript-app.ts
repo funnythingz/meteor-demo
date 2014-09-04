9ea0ec818705d2e5cff66f5e56c844dd03dabf93
funnythingz <funnythingz69+github@gmail.com>
2014-09-04 15:01:42 +0900

    ready TypeScript

---
+++ typescript-app/typescript-app.ts
@@ -0,0 +1,26 @@
+///<reference path="./typings/lib.d.ts"/>
+///<reference path="./typings/meteor.d.ts"/>
+
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault("counter", 0);
+
+  Template['hello'].helpers({
+    counter: function () {
+      return Session.get("counter");
+    }
+  });
+
+  Template['hello'].events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set("counter", Session.get("counter") + 1);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
